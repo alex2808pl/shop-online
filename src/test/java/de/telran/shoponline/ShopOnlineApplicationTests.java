9f79fb99f31cdd80b31b58a33763ff8ad50c0928
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2024-03-19 14:34:17 +0200

    Initial commit

---
+++ src/test/java/de/telran/shoponline/ShopOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.shoponline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
