65caeb009514199bd0e13812b3e27c028e61af11
BondarenkoMM <masya.klas3000@gmail.com>
2024-03-19 14:17:16 +0100

    Created classes: Categoties, Products

---
+++ src/main/java/de/telran/shoponline/entity/Products.java
@@ -0,0 +1,48 @@
+package de.telran.shoponline.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Products {
+
+    @Id
+    @Column(name = "ProductID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productID;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Description")
+    private String description;
+
+    @Column(name = "Price")
+    private double price;
+
+    @Column(name = "CategoryID")
+    private int categoryID;
+
+    @Column(name = "ImageURL")
+    private String imageURL;
+
+    @Column(name = "DiscountPrice")
+    private double discountPrice;
+
+    @Column(name = "CreatedAt")
+    private Timestamp createdAt;
+
+    @Column(name = "UpdatedAt")
+    private Timestamp updatedAt;
+}
