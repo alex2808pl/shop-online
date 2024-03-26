e6098fb79d7c8c83956ca9dec855d7d2f5372eac
Oleksii <achili3f17@gmail.com>
2024-03-26 13:02:50 +0100

    add data

---
+++ src/main/resources/db/changelog/changeSets/inser_User.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (userID,Email, Name, PasswordHash, PhoneNumber, Role) VALUES
+                                                                     (1,'user1@example.com', 'User 1', 'password1', '+1234567890', 'CLIENT'),
+                                                                     (2,'user2@example.com', 'User 2', 'password2', '+9876543210', 'CLIENT'),
+                                                                     (3,'admin@example.com', 'Admin', 'adminpass', '+1112223334', 'ADMIN');
