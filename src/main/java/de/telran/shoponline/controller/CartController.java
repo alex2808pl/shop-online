e781fe60fe4af1e4cb2f23115a2b810cc4b4fec5
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2024-07-10 01:08:12 +0300

    get info from Security Context

--- src/main/java/de/telran/shoponline/controller/CartController.java
+++ src/main/java/de/telran/shoponline/controller/CartController.java
@@ -2,11 +2,13 @@ package de.telran.shoponline.controller;
 
 import de.telran.shoponline.dto.CartDto;
 import de.telran.shoponline.exceptions.NotFoundInDbException;
+import de.telran.shoponline.security.jwt.JwtAuthentication;
 import de.telran.shoponline.service.CartService;
 import org.modelmapper.spi.ErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,6 +20,10 @@ public class CartController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<CartDto> getCartById(@PathVariable Long id) {
+        //Можно получить инфу из токена, которыю мы перед этим извлекли из фильтра и положили в конетекст Security
+        final JwtAuthentication jwtInfoToken = (JwtAuthentication)SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("Controller Cart - "+jwtInfoToken.getUsername());
+        //-------
         CartDto cart = cartService.getCartById(id);
         return new ResponseEntity<>(cart, HttpStatus.OK);
     }
--- src/main/java/de/telran/shoponline/service/CartService.java
+++ src/main/java/de/telran/shoponline/service/CartService.java
@@ -7,8 +7,10 @@ import de.telran.shoponline.entity.Users;
 import de.telran.shoponline.exceptions.NotFoundInDbException;
 import de.telran.shoponline.mapper.Mappers;
 import de.telran.shoponline.repository.CartRepository;
+import de.telran.shoponline.security.jwt.JwtAuthentication;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Supplier;
@@ -20,6 +22,10 @@ public class CartService {
     private final Mappers mappers;
 
     public CartDto getCartById(Long id) {
+        //Можно получить инфу из токена, которыю мы перед этим извлекли из фильтра и положили в конетекст Security
+        final JwtAuthentication jwtInfoToken = (JwtAuthentication) SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("Service - "+jwtInfoToken.getUsername());
+        //-------
         Cart cart = cartRepository.findById(id)
                 .orElseThrow(() -> new NotFoundInDbException("Не найдена корзина с таким Id"));
         CartDto cartDto = mappers.convertToCartDto(cart);
